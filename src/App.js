c87c22e5a700d50063c6222402662466394a190f
627km <rudals_jeon@naver.com>
2023-07-28 01:03:49 +0900

    feat: Create DiaryItem.js, DiaryList.js
    show Diary List at bottom of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "전경민",
+    content: "안녕",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "김철수",
+    content: "안녕1",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "김짱구",
+    content: "안녕2",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "김유리",
+    content: "안녕3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
