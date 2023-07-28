0de40b336365516937b89c83f6ad79044fbea5c2
627km <rudals_jeon@naver.com>
2023-07-28 20:47:59 +0900

    feat: Write new Diary and Add new Diary

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import { useState, useRef } from "react";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "전경민",
-    content: "안녕",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "김철수",
-    content: "안녕1",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "김짱구",
-    content: "안녕2",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "김유리",
-    content: "안녕3",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "전경민",
+//     content: "안녕",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "김철수",
+//     content: "안녕1",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "김짱구",
+//     content: "안녕2",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "김유리",
+//     content: "안녕3",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  // 새로운 일기를 추가하는 함수
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]); // 새로운 일기가 제일 윗부분에 위치하도록
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
